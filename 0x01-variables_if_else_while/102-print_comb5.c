93de26dbfdc53126acbf183e51ef47952b0e1225
Death-thekidd <ohiemidivine7@gmail.com>
2022-12-09 10:49:08 -0800

    [update]: added C program 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,24 +7,31 @@
  */
 int main(void)
 {
-	int i, j, k, l;
+	int i, j, k, l, comp1, comp2;
 
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j < 10; j++)
 		{
-			for (k = i; k < 10; k++)
+			for (k = 0; k < 10; k++)
 			{
-				for (l = k ? j : j + 1; l < 10; l++)
+				for (l = 0; l < 10; l++)
 				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(' ');
-					putchar(k + '0');
-					putchar(l + '0');
-					if (i != 9 || j != 8 || k != 9 || l != 9)
+					comp1 = (i * 10) + j;
+					comp2 = (k * 10) + l;
+					if (comp1 < comp2)
 					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(' ');
+						putchar(k + '0');
+						putchar(l + '0');
+
+					if (i == 9 && j == 8 && k == 9 && l == 9)
+					{
+						break;
+					}
 						putchar(',');
 						putchar(' ');
 					}
