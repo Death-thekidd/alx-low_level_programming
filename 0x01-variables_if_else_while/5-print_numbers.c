91b24be297146200937f4760b9181b3a550318ff
Death-thekidd <ohiemidivine7@gmail.com>
2022-12-09 03:39:11 -0800

    [update]: added C program 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: Always 0 (Success)
+ * Description: Determines either greater than 5, is less than 6, or is 0.
+ */
+int main(void)
+{
+	printf("0123456789\n");
+
+	return (0);
+}
