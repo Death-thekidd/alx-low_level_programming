4754638d2dd729fe67e24f36803ce08491e02055
Death-thekidd <ohiemidivine7@gmail.com>
2022-12-13 03:37:05 -0800

    [update]: added function 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - prints the alphabet, in lowercase,
+* followed by a new line.
+* Return: void
+*/
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		_putchar(i);
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,5 @@
 # 0x02-functions_nested_loops
 ## 0-putchar.c
 A C  program that prints _putchar, followed by a new line.
+## 1-alphabet.c
+A function that prints the alphabet, in lowercase, followed by a new line.
