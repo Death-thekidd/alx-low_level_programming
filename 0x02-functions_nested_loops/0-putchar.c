a166d13470c8b521099ed8e46a590706670309f7
Death-thekidd <ohiemidivine7@gmail.com>
2022-12-13 03:24:27 -0800

    [fix]: betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,8 +10,8 @@ int main(void)
 	int arr[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int i;
 	long int size = sizeof(arr) / sizeof(int);
-	
-	for(i = 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 		_putchar(arr[i]);
 	_putchar('\n');
 	return (0);
