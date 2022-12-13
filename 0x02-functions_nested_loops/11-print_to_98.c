e8a5bdf16def0b397599ed097915ac5be70794f8
Death-thekidd <ohiemidivine7@gmail.com>
2022-12-13 15:35:37 -0800

    [feat]: added C program 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+void otherwise(int n, int i);
 /**
 * print_to_98 - checks for lowercase character.
 * @n: The number to count down or up
@@ -7,11 +8,26 @@
 */
 void print_to_98(int n)
 {
+	int i = 0;
+
 	if (n >= 98)
 	{
 		while (n >= 98)
 		{
-			_putchar(n + 48);
+			if (n <= 9)
+				_putchar(n + 48);
+			else if (n > 9 && n <= 99)
+			{
+				_putchar((n / 10) + 48);
+				_putchar((n % 10) + 48);
+			} else if (n > 99)
+			{
+				_putchar((n / 100) + '0');
+				i = n % 100;
+				_putchar((i / 10) + '0');
+				i = n % 10;
+				_putchar(i + '0');
+			}
 			if (n != 98)
 			{
 				_putchar(',');
@@ -21,15 +37,50 @@ void print_to_98(int n)
 		}
 	} else
 	{
-		while (n <= 98)
+		otherwise(n, i);
+	}
+	_putchar('\n');
+}
+/**
+* otherwise - Action to perform if n is less
+* than 98
+* @n: number to check
+* @i: temp var
+*
+* Return: void
+*/
+void otherwise(int n, int i)
+{
+	while (n <= 98)
+	{
+		i = n < 0 ? -n : n;
+		if (i <= 9)
 		{
-			_putchar(n + 48);
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			n++;
+			if (n < 0)
+				_putchar('-');
+			_putchar(i + 48);
 		}
+		else if (i > 9 && i <= 99)
+		{
+			if (n < 0)
+				_putchar('-');
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+		} else if (i > 99)
+		{
+			if (n < 0)
+				_putchar('-');
+			_putchar((i / 100) + '0');
+			i = i % 100;
+			_putchar((i / 10) + '0');
+			i = i % 10;
+			_putchar(i + '0');
+		}
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		n++;
 	}
 }
