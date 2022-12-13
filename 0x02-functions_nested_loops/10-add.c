d22fb003821b40c9574d203ac957859d9f46c226
Death-thekidd <ohiemidivine7@gmail.com>
2022-12-13 13:19:47 -0800

    [update]: added C program 9-times_table.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - checks for lowercase character.
+* @a: The integer to add
+* @b: The integer to add
+*
+* Return: The addition of a and b 
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
