2dd77795b57b5a0bb28cb4bd633ce79a2cc46d36
Death-thekidd <ohiemidivine7@gmail.com>
2022-12-13 01:55:27 -0800

    [update]: added C program  0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry Point
+* Return: Always (0) Success
+* Description: Prints _putchar, followed by a new line.
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# 0x02-functions_nested_loops
+## 0-putchar.c
+A C  program that prints _putchar, followed by a new line.
