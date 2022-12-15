6dc56159f3e903e753d6f6902febf91b9e3e27b5
Death-thekidd <ohiemidivine7@gmail.com>
2022-12-15 07:11:04 -0800

    [update] added function 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - Prints the numbers, from 0 to 9,
+* followed by a new line.
+* @c: parameter to check
+*
+* Return: void
+*/
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -5,3 +5,5 @@ A function that checks for uppercase character.
 A function that checks for a digit (0 through 9).
 ## 2-mul.c
 A function that multiplies two integers.
+## 3-print_numbers.c
+A function that prints the numbers, from 0 to 9, followed by a new line.
