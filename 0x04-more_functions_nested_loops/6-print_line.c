739cc9860d162dedf1750d4226d5ca49f523616f
Death-thekidd <ohiemidivine7@gmail.com>
2022-12-15 08:23:15 -0800

    [update] added function 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,10 +10,13 @@ void print_line(int n)
 {
 	int i;
 	int m = n > 0 ? n : 0;
-	
+
 	if (m <= 0)
 		_putchar('\n');
-	for (i = 0; i < m; i++)
-		_putchar(95);
-	_putchar('\n');
+	else
+	{
+		for (i = 0; i < m; i++)
+			_putchar(95);
+		_putchar('\n');
+	}
 }
