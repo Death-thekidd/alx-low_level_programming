3cfa704efd2ec39d0c162d31855417363a22f330
Death-thekidd <ohiemidivine7@gmail.com>
2022-12-22 07:16:05 -0800

    [update]: added new function

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,16 +7,19 @@
 */
 char *rot13(char *str)
 {
-	int c;
+	int c, i;
 	char cc;
+	char arr1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char arr2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (c = 0; *(str + c) != 0; c++)
 	{
 		cc = *(str + c);
-		if ((cc > 64 && cc < 78) || (cc > 96 && cc < 110))
-			*(str + c) += 13;
-		else if ((cc > 77 && cc < 91) || (cc > 109 && cc < 123))
-			*(str + c) -= 13;
+		for (i = 0; i < 52; i++)
+		{
+			if ((cc == *(arr1 + i)))
+				*(str + c) = *(arr2 + i);
+		}
 	}
 	return (str);
 }
