c6c1b5810a4d3c0b124df43987070765466237c4
Death-thekidd <ohiemidivine7@gmail.com>
2023-01-04 08:51:47 -0800

    [update]: added new function _print_rev_recursion

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+# 0x08-recursion
+## 1-print_rev_recursion.c
+A function that prints a string in reverse.
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,9 @@
+#ifndef _main_h_
+#define _main_h_
+
+#include <string.h>
+
+int _putchar(char);
+void _print_rev_recursion(char *);
+
+#endif
