3955df2915b36b1a85ed084e655b19d7a45a3555
Death-thekidd <ohiemidivine7@gmail.com>
2023-01-06 12:25:51 -0800

    [update]: added C source file 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints the number of arguments passed into it.
+* @argc: number of command line arguments
+* @argv: array of command line arguments
+*
+* Return: 0
+*/
+int main(int argc, char __attribute__ ((unused)) **argv)
+{
+	printf("%d\n", argc);
+	exit(EXIT_SUCCESS);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,5 @@
 # 0x0A-argc_argv
 ## 0-whatsmyname.c
 > A program that prints its name, followed by a new line.
+## 1-args.c
+> A program that prints the number of arguments passed into it.
