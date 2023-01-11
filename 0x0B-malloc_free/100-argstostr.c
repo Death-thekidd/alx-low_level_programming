f2f3b1edb4c7493758759751633c5ca52df77047
Death-thekidd <ohiemidivine7@gmail.com>
2023-01-10 18:56:54 -0800

    [update]: adde new function argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: number of arguments
+ * @av: array of arguments
+ *
+ * Return: Always 0.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, l;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 1, k = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != 0; j++)
+			k++;
+	}
+	k = k + (1 * (ac - 1));
+	str = malloc(sizeof(char) * k);
+	if (str == NULL)
+		return (NULL);
+	for (i = 1, l = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != 0; j++, l++)
+			str[l] = av[i][j];
+		str[l++] = '\n';
+	}
+	return (str);
+}
