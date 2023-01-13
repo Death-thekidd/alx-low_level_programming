5fc5e2a887bfc9ae915bcd6a1324317e2f52b85f
Death-thekidd <ohiemidivine7@gmail.com>
2023-01-12 16:55:00 -0800

    [update]: added new program 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
-		printf("Error\n"), exit(98);
+		exit(EXIT_FAILURE);
 	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
