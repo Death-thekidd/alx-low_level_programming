7b08e2045931cfb93436b34846055ecd1bbbfc33
Death-thekidd <ohiemidivine7@gmail.com>
2023-01-16 15:11:01 -0800

    [update]: added new header file dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,8 @@
+# 0x0E-structures_typedef
+## dog.h
+> Define a new type struct dog with the following elements:
+```
+    name, type = char *
+    age, type = float
+    owner, type = char *
+```
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _dog_h
+#define _dog_h
+
+/**
+* struct dog -  type struct dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
