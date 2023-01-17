2e5ba2804b79ddeed8327cb0d010a0c30a3e49e9
Death-thekidd <ohiemidivine7@gmail.com>
2023-01-16 17:04:07 -0800

    [update]: added new function new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,14 +11,34 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t new_dog;
-	dog_t *ptr;
+	int nname, nowner, i;
+	dog_t *new_dog;
 
-	ptr = malloc(sizeof(new_dog));
-	if (ptr == NULL)
-		return(NULL);
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;
-	return (ptr);
+	new_dog = malloc(sizeof(*new_dog));
+	if (new_dog == NULL || !(name) || !(owner))
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	for (nname = 0; name[nname]; nname++)
+		;
+	for (nowner = 0; owner[nowner]; nowner++)
+		;
+	new_dog->name = malloc(nname + 1);
+	new_dog->owner = malloc(nowner + 1);
+	if (!(new_dog->name) || !(new_dog->owner))
+	{
+		free(new_dog->owner);
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; i < nname; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
+	new_dog->age = age;
+	for (i = 0; i < nowner; i++)
+		new_dog->owner[i] = owner[i];
+	new_dog->owner[i] = '\0';
+	return (new_dog);
 }
