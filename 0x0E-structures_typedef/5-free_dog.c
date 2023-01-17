9ebf0fe95b1cbff3b80b4d6b8c31ef40f1c39746
Death-thekidd <ohiemidivine7@gmail.com>
2023-01-16 17:13:36 -0800

    [update]: added new function free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - creates a new dog.
+ * @d: pointer to dog struct
+ *
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
