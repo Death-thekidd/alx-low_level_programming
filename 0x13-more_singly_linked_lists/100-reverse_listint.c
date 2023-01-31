5a050231fffd71fa84ca1b3bd7bfcb72d251a207
Death-thekidd <ohiemidivine7@gmail.com>
2023-01-31 15:51:59 +0100

    [update]: removed a variable

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,15 +9,14 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev   = NULL;
-	listint_t *current = *head;
 	listint_t *next;
 
-	while (current != NULL)
+	while (*head != NULL)
 	{
-		next  = current->next;
-		current->next = prev;   
-		prev = current;
-		current = next;
+		next  = (*head)->next;
+		(*head)->next = prev;   
+		prev = *head;
+		*head = next;
 	}
 	*head = prev;
 	return (*head);
