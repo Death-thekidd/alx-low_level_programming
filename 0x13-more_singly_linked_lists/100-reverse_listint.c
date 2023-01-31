33df0983c9f6226dc4ad1ba9c8c361f61b786609
Death-thekidd <ohiemidivine7@gmail.com>
2023-01-31 17:56:55 +0100

    [fix]: betty style

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -14,7 +14,7 @@ listint_t *reverse_listint(listint_t **head)
 	while (*head != NULL)
 	{
 		next  = (*head)->next;
-		(*head)->next = prev;   
+		(*head)->next = prev;
 		prev = *head;
 		*head = next;
 	}
