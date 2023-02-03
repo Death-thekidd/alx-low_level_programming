42c6af4fde7478bd6354263005884324d52e6691
Death-thekidd <ohiemidivine7@gmail.com>
2023-02-03 05:34:24 +0100

    [fix]: used bitwise operators

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,57 +1,38 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: binary number
- *
- * Return: converted number
- */
+* binary_to_uint - converts a binary number to an
+* unsigned int.
+* @b: binary.
+*
+* Return: unsigned int.
+*/
 unsigned int binary_to_uint(const char *b)
 {
-	const char *a;
-	int chk, len, i;
-	unsigned int num, digit;
+	unsigned int ui;
+	int len, base_two;
 
-	a = b;
-	for (len = 0; *a != 0; a++, len++)
-		;
-	chk = checkBArr(b);
-	if (chk || (b == NULL))
+	if (!b)
 		return (0);
-	num = _atoi(b, len);
-	for (i = 1, digit = 0; num != 0; i *= 2, num /= 10)
-		digit += (num % 10) * i;
-	return (digit);
-}
 
-/**
- * checkBArr - checks for foreign characters
- * @b: binary number
- *
- * Return: boolean
- */
-int checkBArr(const char *b)
-{
-	for (; *b != 0; b++)
-		if (*b != 49 && *b != 48)
-			return (1);
+	ui = 0;
 
-	return (0);
-}
+	for (len = 0; b[len] != '\0'; len++)
+		;
 
-/**
- * _atoi - converts a string to an integer
- * @b: string
- * @l: length of string
- *
- * Return: converted number
- */
-unsigned int _atoi(const char *b, int l)
-{
-	int place, i;
-	unsigned int num;
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
 
-	for (i = l - 1, place = 1, num = 0; i >= 0; place *= 10, i--)
-		num += ((b[i] - 48) * place);
-	return (num);
+		if (b[len] & 1)
+		{
+			ui += bas
+			e_two;
+		}
+	}
+
+	return (ui);
 }
