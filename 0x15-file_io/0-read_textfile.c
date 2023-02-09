0ac8b6d072581a84ce709ab7bb0834892b55e70b
Death-thekidd <ohiemidivine7@gmail.com>
2023-02-09 22:58:16 +0100

    [update]: added new files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * read_ttextfile - reads a text file and prints it to the POSIX standard output.
+ * @filename: name of file to create
+ * @letters : number of letters to read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int val;
+	ssize_t i;
+	char *buffer, *buf;
+
+	buffer = malloc(letters);
+	val = open(filename, O_RDONLY ,0666);
+	if (!val || buffer == NULL)
+		return (0);
+	read(val, buffer, letters);
+	for (i = 0, buf = buffer; *buf != 0; buf++, i++)
+		printf("%c", *buf);
+	close(val);
+	free(buffer);
+	return(i);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+# 0x15-file_io
+
+## 0-read_textfile.c
+
+> A function that reads a text file and prints it to the POSIX standard output.
