3b0bea4cca3df69d998e11a0cc940605b9c8941e
Death-thekidd <ohiemidivine7@gmail.com>
2023-03-20 23:04:55 +0100

    [update]; added Dynamic

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,15 +8,17 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev   = NULL;
-	listint_t *next;
+	listint_t *prev_node = NULL;
+	listint_t *next_node;
 
-	while (*head != NULL)
+	next_node = *head;
+
+	while (next_node != NULL)
 	{
-		next  = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
-		*head = next;
+		next_node  = next_node->next;
+		(*head)->next = prev_node;
+		prev_node = *head;
+		*head = next_node;
 	}
 	*head = prev;
 	return (*head);
