867eb651d06d3838c72213d98339a391bd6fae30
deaththekidd <ohiemidivine7@gmail.com>
2023-06-17 02:58:54 +0100

    [update]

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,57 +10,25 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, f, l1, l2, ttl, n_chk;
-	char *concat, *verif1, *verif2;
+	unsigned int i, f, l1, l2, ttl;
+	char *concat;
 
-	verif1 = __strdup(s1), verif2 = __strdup(s2);
-	for (i = 0; verif1[i] != '\0'; i++)
-		;
-	l1 = i;
-	for (i = 0; verif2[i] != '\0'; i++)
-		;
-	l2 = i;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	l1 = strlen(s1);
+	l2 = strlen(s2);
 	if (n >= l2)
-		n_chk = l2;
-	else
-		n_chk = n;
-	ttl = l1 + n_chk + 1;
-	concat = malloc(sizeof(char) * ttl);
+		n = l2;
+	ttl = l1 + n;
+	concat = malloc(sizeof(char) * (ttl + 1));
 	if (concat == NULL)
 		return (NULL);
-	for (i = 0; verif1[i] != '\0'; i++)
-		concat[i] = verif1[i];
-	for (f = 0; f < n_chk; f++, i++)
-		concat[i] = verif2[f];
+	for (i = 0; i < l1; i++)
+		concat[i] = s1[i];
+	for (f = 0; f < n; f++, i++)
+		concat[i] = s2[f];
 	concat[i] = '\0';
-	if (*verif1 != '\0')
-		free(verif1);
-	if (*verif2 != '\0')
-		free(verif2);
 	return (concat);
-}
-
-/**
- * __strdup - returns copied string
- * @str: string to copy
- *
- * Return: pointer
-*/
-char *__strdup(char *str)
-{
-	int i, size;
-	char *arr;
-
-	if (str == NULL)
-		return ("");
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	size = i++;
-
-	arr = malloc(sizeof(char) * size);
-	if (arr == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		arr[i] = str[i];
-	return (arr);
 }
