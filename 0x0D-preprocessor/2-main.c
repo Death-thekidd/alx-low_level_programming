f7022616e64e893572c4e8fad776ecdb1bfdd119
deaththekidd <ohiemidivine7@gmail.com>
2023-06-21 13:40:14 +0100

    [update]

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints the name of the file it was compiled from, followed by a new line.
+ * main - prints the name of the file it was compiled
+ * from, followed by a new line.
  *
  * Return: 0
 */
