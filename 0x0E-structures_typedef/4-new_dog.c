16e6555991023110a032f9f1bd469b7c25972890
deaththekidd <ohiemidivine7@gmail.com>
2023-06-21 13:31:17 +0100

    [update]

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,8 +30,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	strcpy(new_dog->name, name);
-	strcpy(new_dog->owner, owner);
+	strcpy(dog->name, name);
+	strcpy(dog->owner, owner);
 	dog->age = age;
 
 	return (dog);
