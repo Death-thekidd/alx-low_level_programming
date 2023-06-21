a37b4d5d8ceea90772ee822ff09b60d8e2d5ed77
deaththekidd <ohiemidivine7@gmail.com>
2023-06-21 11:59:17 +0100

    [update]

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: the created struct
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
